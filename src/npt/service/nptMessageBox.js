5be0ff707afd1663ff3a57f72bbb63092cf97fb9
CKHuang <ckhuang326@icloud.com>
2015-12-04 17:53:38 +0800

    1.增加nptMessageBox功能

--- src/npt/service/nptMessageBox.js
+++ src/npt/service/nptMessageBox.js
@@ -19,29 +19,56 @@ angular.module("ui.neptune.service.messageBox", ['ui.bootstrap'], function ($com
         };
     });
 })
-.factory("messageBoxFactory", function ($uibModal) {
+    .factory("nptMessageBox", function ($uibModal) {
         var factory = {};
 
-        function Modal(options, modalResult) {
-            this.options = options;
-            this.modalResult = modalResult;
+        function Modal(options, uibModal) {
+            var self = this;
+            this._options = options;
+            this._uibModal = uibModal;
 
-            // 注册弹出框弹出后点击事件
-            this.modalResult.then(function (actionCode, scope) {
-                console.log("点击.." + actionCode + ";" + scope);
-            }, function () {
-                console.log("点击了取消..");
+            this.action = {};
+            angular.forEach(this._options.action, function (action, key) {
+                self.action[key] = function () {
+                    self._uibModal.close({
+                        type: key
+                    });
+                };
             });
         }
 
-        Modal.prototype.then = function () {
-            if (arguments.length === 0) {
-                return;
+        Modal.prototype.uibModal = function () {
+            return this._uibModal;
+        };
+
+        Modal.prototype.updateScope = function(name,data) {
+            if (arguments.length == 1) {
+                this._options.scope = name;
+            } else if (arguments.length > 1) {
+                this._options.scope[name] = data;
             }
         };
 
+        Modal.prototype.on = function (actionName, fn) {
+            if (!actionName || !fn) {
+                throw new Error("无效的入参：" + actionName + "," + fn);
+            }
+            this._options.action[actionName] = this._options.action[actionName] || {};
+            this._options.action[actionName].listens = this._options.action[actionName].listens || [];
+            this._options.action[actionName].listens.push(fn);
+
+        };
+
+
         factory.open = function (options) {
-            var result = $uibModal.open({
+            var defaultOptions = {
+                showCancel: false
+            };
+            options = angular.copy(options);
+            options = angular.extend(defaultOptions, options);
+            options.action = options.action || {};
+            options.scope = options.scope || {};
+            var openOptions = {
                 animation: true,
                 templateUrl: '/template/message-box/message-box.html',
                 controller: 'messageBoxController',
@@ -53,20 +80,26 @@ angular.module("ui.neptune.service.messageBox", ['ui.bootstrap'], function ($com
                         return deferd.promise;
                     }
                 }
-            }).result;
-            var modal = new Modal(options, result);
+            };
+            openOptions = angular.extend(openOptions, options.modal || {});
+            var uibModal = $uibModal.open(openOptions);
+            var modal = new Modal(options, uibModal);
+            uibModal.opened.then(function () {
+                if (options.onRegisterApi) {
+                    options.onRegisterApi(modal);
+                }
+            });
             return modal;
         };
 
         return factory;
     })
     .controller("messageBoxController",
-    function ($scope, $uibModalInstance, $compile, $templateCache, modalOptions) {
+    function ($scope, $uibModalInstance, $q, $injector, $templateCache, modalOptions) {
         var vm = this;
 
-        vm.modalOptions = angular.copy(modalOptions);
-        $scope.$$ms = {};
-        angular.extend($scope.$$ms, vm.modalOptions.scope || {});
+        vm.modalOptions = modalOptions;
+        $scope.$$ms = vm.modalOptions.scope;
 
         vm.modalContent = vm.modalOptions.content;
         if (vm.modalOptions.templateUrl) {
@@ -74,17 +107,46 @@ angular.module("ui.neptune.service.messageBox", ['ui.bootstrap'], function ($com
         }
 
         // function assignment
-        vm.btnClick = btnClick;
-        vm.cancel = cancel;
-        // function definition
-        function btnClick(type) {
+        vm.btnClick = function (type) {
             $uibModalInstance.close({
-                type: type,
-                scope: $scope.$$ms
+                type: type
             });
+        };
+
+        // 执行指定按钮类型的监听器
+        function fireListener(type) {
+            var modalResult = {
+                type: type,
+                "scope": $scope.$$ms
+            };
+            var deferd = $q.defer();
+            var promise = deferd.promise;
+            deferd.resolve(modalResult);
+            var promisesArr = [promise];
+            vm.modalOptions.action[type] = vm.modalOptions.action[type] || {};
+            var listens = vm.modalOptions.action[type].listens || [];
+            //配置中的listen
+            angular.forEach(listens, function (listen) {
+                promisesArr.push($q.when($injector.invoke(listen, this, {
+                    "modalResult": modalResult
+                })));
+            });
+            return $q.all(promisesArr);
         }
 
-        function cancel() {
-            $uibModalInstance.dismiss('cancel');
-        }
+        // 监听窗口关闭事件
+        $scope.$on("modal.closing", function (event, result) {
+            // result 为true时为dismiss事件，不处理
+            if (result !== true) {
+                event.preventDefault(); // 阻止窗口关闭
+                var actionType = "cancel";
+                if (angular.isObject(result)) {
+                    actionType = result.type;
+                }
+                fireListener(actionType).then(function () {
+                    $uibModalInstance.dismiss(true);
+                });
+            }
+
+        });
     });
