b2a0b50c0ae3da4c845bb53077f1f8d883672d34
CKHuang <ckhuang326@icloud.com>
2016-01-04 17:59:30 +0800

    删除不必要的maskId

--- src/npt/directive/nptLoading.js
+++ src/npt/directive/nptLoading.js
@@ -15,13 +15,8 @@ angular.module("ui.neptune.directive.nptLoading", [])
             controller: "NptLoadingController",
             template:"",
             link : function(scope, element, attrs) {
-                // maskID
-                scope.maskId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                    return v.toString(16);
-                }).replace(/-/g,"");
                 scope.loadingMessage = "正在加载...";
-                var div = $compile('<div ng-show="showLoadingMask" id="'+scope.maskId+'" style="position: absolute; top: 0px; filter: alpha(opacity=60); ' +
+                var div = $compile('<div ng-show="showLoadingMask" style="position: absolute; top: 0px; filter: alpha(opacity=60); ' +
                     'background-color: #777;z-index: 9999; left: 0px;opacity:0.4; -moz-opacity:0.4;">' +
                     '<div style="text-align: center;color: white;font-weight: 700;font-size: 15px;"><img src="/image/loading.gif" />{{loadingMessage}}</div>' +
                     '</div>')(scope);
